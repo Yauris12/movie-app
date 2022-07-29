766b5f3d5724bcba75d2141d6c92dcaeaec10a6c
Yauris12 <alexisaube28@gmail.com>
2022-07-28 23:32:47 -0500

    add env

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
-  baseUrl: process.env.REACT_APP_BASE_URL,
-  apiKey: process.env.REACT_APP_API_KEY,
+  baseUrl: process.env.REACT_APP_BASE_URL || 'https://api.themoviedb.org/3/',
+  apiKey: process.env.REACT_APP_API_KEY || '1b3c345322f1155f1c7c52185d692706',
   originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
   w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
 }
