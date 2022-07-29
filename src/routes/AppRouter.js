38021fa4066c51f117346c31bcc2043660534d00
Yauris12 <alexisaube28@gmail.com>
2022-07-28 23:48:45 -0500

    change Router

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import { Home, Detail, Movies, Error, Favorite } from '../pages'
 import SharedLayout from './SharedLayout'
 
